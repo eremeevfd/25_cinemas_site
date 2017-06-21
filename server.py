de7e243c77b03e19b749c343296f7ecbd09d3e6e
eremeevfd <dartfedor@mail.ru>
2017-06-21 13:06:09 +0300

    heroku deploying

--- server.py
+++ server.py
@@ -1,10 +1,26 @@
 from flask import Flask, render_template
+from werkzeug.contrib.cache import FileSystemCache
+from cinemas import get_top_films
+import os
+import threading
+
 
 app = Flask(__name__)
+lock = threading.Lock()
+app.config['DEBUG'] = True
+
 
 @app.route('/')
 def films_list():
-    return render_template('films_list.html')
+    current_dir = os.path.dirname(__file__)
+    cache_dir = current_dir + '/cache/'
+    cached_films = FileSystemCache(cache_dir=cache_dir, default_timeout=12 * 60 * 60)
+    if not cached_films.get('cinemas'):
+        with lock:
+            cinemas = get_top_films()
+        cached_films.set('cinemas', cinemas)
+    return render_template('films_list.html', cinemas=cached_films.get('cinemas'))
 
 if __name__ == "__main__":
+    empty_list_info = []
     app.run()
